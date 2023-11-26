9fc69b1578919d38fe47059076d8fd1474ca9703
Bharath R <114679542+BHARATHQUENTIN@users.noreply.github.com>
2023-11-26 10:37:07 +0530

    changed in profile.php

--- php/profile.php
+++ php/profile.php
@@ -41,10 +41,10 @@ function insertOrUpdateUserData($userEmail, $name, $contact, $age, $dateOfBirth,
 
         $existingDocument = $collection->findOne(['email' => $userEmail]);
 
-        if ($existingDocument) {
-            // Create an array to store update data
-            $updateData = [];
+        // Create an array to store update data
+        $updateData = [];
 
+        if ($existingDocument) {
             // Always include the existing fields in the update data
             $updateData['contact'] = $existingDocument['contact'];
             $updateData['age'] = $existingDocument['age'];
@@ -57,18 +57,18 @@ function insertOrUpdateUserData($userEmail, $name, $contact, $age, $dateOfBirth,
             }
 
             // Calculate age based on date of birth
-            if (isset($dateOfBirth) && $dateOfBirth !== "NA" &&$dateOfBirth!== '') {
+            if (isset($dateOfBirth) && $dateOfBirth !== "NA" && $dateOfBirth !== '') {
                 $dob = new DateTime($dateOfBirth);
                 $today = new DateTime();
                 $age = $dob->diff($today)->y;
                 $updateData['age'] = $age;
             }
 
-            if (isset($dateOfBirth) && $dateOfBirth !== "NA" && $dateOfBirth !=='') {
+            if (isset($dateOfBirth) && $dateOfBirth !== "NA" && $dateOfBirth !== '') {
                 $updateData['dateOfBirth'] = $dateOfBirth;
             }
 
-            if (isset($address) && $address !== "NA" && $address !=='') {
+            if (isset($address) && $address !== "NA" && $address !== '') {
                 $updateData['address'] = $address;
             }
 
@@ -85,85 +85,25 @@ function insertOrUpdateUserData($userEmail, $name, $contact, $age, $dateOfBirth,
                 $response['error'] = 'Failed to update document in MongoDB. No changes made.';
             }
         } else {
-            function insertOrUpdateUserData($userEmail, $name, $contact, $age, $dateOfBirth, $address)
-            {
-                $response = ['success' => false];
-                try {
-                    $collection = connectToMongoDB();
-            
-                    $existingDocument = $collection->findOne(['email' => $userEmail]);
-            
-                    if ($existingDocument) {
-                        // Create an array to store update data
-                        $updateData = [];
-            
-                        // Always include the existing fields in the update data
-                        $updateData['contact'] = $existingDocument['contact'];
-                        $updateData['age'] = $existingDocument['age'];
-                        $updateData['dateOfBirth'] = $existingDocument['dateOfBirth'];
-                        $updateData['address'] = $existingDocument['address'];
-            
-                        // Update with new values if provided
-                        if (isset($contact) && $contact !== "NA") {
-                            $updateData['contact'] = $contact;
-                        }
-            
-                        // Calculate age based on date of birth
-                        if (isset($dateOfBirth) && $dateOfBirth !== "NA") {
-                            $dob = new DateTime($dateOfBirth);
-                            $today = new DateTime();
-                            $age = $dob->diff($today)->y;
-                            $updateData['age'] = $age;
-                        }
-            
-                        if (isset($dateOfBirth) && $dateOfBirth !== "NA") {
-                            $updateData['dateOfBirth'] = $dateOfBirth;
-                        }
-            
-                        if (isset($address) && $address !== "NA") {
-                            $updateData['address'] = $address;
-                        }
-            
-                        $updateResult = $collection->updateOne(
-                            ['email' => $userEmail],
-                            ['$set' => $updateData]
-                        );
-            
-                        if ($updateResult->getModifiedCount() > 0) {
-                            $response['success'] = true;
-                            $response['message'] = 'Document updated in MongoDB successfully.';
-                            $response['age'] = $age; // Include calculated age in the response
-                        } else {
-                            $response['error'] = 'Failed to update document in MongoDB. No changes made.';
-                        }
-                    } else {
-                        // If the document doesn't exist, insert a new one
-                        $newDocument = [
-                            'email' => $userEmail,
-                            'firstname' => $name,
-                            'contact' => isset($contact) ? $contact : "NA",
-                            'age' => $age, // Use the provided age or the calculated age
-                            'dateOfBirth' => isset($dateOfBirth) ? $dateOfBirth : "NA",
-                            'address' => isset($address) ? $address : "NA",
-                        ];
-            
-                        $result = $collection->insertOne($newDocument);
-            
-                        if ($result->getInsertedCount() > 0) {
-                            $response['success'] = true;
-                            $response['message'] = 'Document inserted in MongoDB successfully.';
-                            $response['age'] = $age; // Include age in the response
-                        } else {
-                            $response['error'] = 'Failed to insert document in MongoDB.';
-                        }
-                    }
-                } catch (Exception $e) {
-                    $response['error'] = 'MongoDB Exception: ' . $e->getMessage();
-                }
-            
-                return $response;
+            // If the document doesn't exist, insert a new one
+            $newDocument = [
+                'email' => $userEmail,
+                'firstname' => $name,
+                'contact' => isset($contact) ? $contact : "NA",
+                'age' => $age, // Use the provided age or the calculated age
+                'dateOfBirth' => isset($dateOfBirth) ? $dateOfBirth : "NA",
+                'address' => isset($address) ? $address : "NA",
+            ];
+
+            $result = $collection->insertOne($newDocument);
+
+            if ($result->getInsertedCount() > 0) {
+                $response['success'] = true;
+                $response['message'] = 'Document inserted in MongoDB successfully.';
+                $response['age'] = $age; // Include age in the response
+            } else {
+                $response['error'] = 'Failed to insert document in MongoDB.';
             }
-            
         }
     } catch (Exception $e) {
         $response['error'] = 'MongoDB Exception: ' . $e->getMessage();
